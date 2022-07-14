1ff7aa246436dfcfab1dd9cd89dc796bef025bbb
MinJi Kang <86655689+100100001001@users.noreply.github.com>
2022-07-15 00:14:06 +0900

    Add InventoryUI

---
+++ Assets/InventoryUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    //public GameObject inventoryPanel; // Inventory UI 오브젝트가 담길 변수
+
+    //bool activeInventory = false; // Inventory UI 오브젝트의 Open 또는 Close 상태를 표현할 bool형 변수
+
+    void Start()
+    {
+        //// inventoryPanel의 활성화 상태를 activeInventory값(비활성)으로 변경
+        //inventoryPanel.SetActive(activeInventory);
+    }
+
+    void Update()
+    {
+        
+    }
+}
